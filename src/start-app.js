3639ff09931e9f251034e1ca4741acd133eab7c0
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2018-06-04 18:14:24 +0200

    Remove default variables in config and add "required"

--- src/start-app.js
+++ src/start-app.js
@@ -1,6 +1,22 @@
 'use strict';
 
+const config = require('./config/config');
 const { createServer } = require('./server');
+
+try {
+  config.validate({ allowed: 'strict' });
+}
+catch (error) {
+  const configErrorMessages = error.message.split('\n');
+  configErrorMessages.forEach((message) => {
+    console.error('Bad config -', message);
+  });
+
+  // 9 - Invalid Argument
+  // https://nodejs.org/api/process.html#process_exit_codes
+  console.error('App STOPPED because of configuration errors');
+  process.exit(9);
+}
 
 createServer()
   .then(async (server) => {
