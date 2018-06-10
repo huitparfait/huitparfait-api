3657669d526fc8f993cfe0e33e44cc2733612be3
Maxime Ghignet <maxime.ghignet@gmail.com>
2018-06-10 13:11:54 +0200

    Make default errors more generic

--- src/utils/default-error-handling.js
+++ src/utils/default-error-handling.js
@@ -6,7 +6,7 @@ const DatabaseNotFoundError = require('../database-pool').DatabaseNotFoundError;
 module.exports = function (request) {
   return function (e) {
     if (e instanceof DatabaseNotFoundError) {
-      return Boom.notFound(`Group ${request.params.id} was not found`);
+      return Boom.notFound(e.message, request.params);
     }
     throw e;
   };
